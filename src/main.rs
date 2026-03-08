5e41cc96e4bff968bc719612c2c3625b87eeaa2b
Lazu13 <tomek.laz.13@gmail.com>
2026-03-08 21:30:37 +0100

    updated main method

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use payment_engine::dto::transaction_dto::TransactionDTO;
 use payment_engine::model::my_error::MyError;
 use payment_engine::model::my_result::MyResult;
-use payment_engine::model::transaction::Transaction;
+use payment_engine::service::payment_engine::PaymentEngine;
 use payment_engine::utils::csv_reader::read_csv;
+use payment_engine::utils::output_writer::write_output;
 use std::env;
 
 #[tokio::main]
@@ -17,11 +18,16 @@ async fn run() -> MyResult<()> {
     let path: String = env::args().nth(1).ok_or(MyError::MissingArgument)?;
 
     let records = read_csv::<TransactionDTO>(path.as_str())?;
+    let mut engine = PaymentEngine::new(100, 10);
 
     for dto in records.flatten() {
-        let txn: Transaction = dto.try_into()?;
-        println!("Client {}", txn.client())
+        if let Ok(txn) = dto.try_into() {
+            engine.process(txn).await?;
+        }
     }
+
+    let states = engine.shutdown().await?;
+    write_output(states);
 
     Ok(())
 }
